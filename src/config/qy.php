fa95bea4e51b805b3bb808fb3bd7e2f4cd1c70b1
eva <1397750928@qq.com>
2022-12-23 15:01:59 +0800

    upt config

---
+++ src/config/qy.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    // 企业 ID
+    'corp_id'                => '',
+    // 秘钥
+    'corp_secret'            => '',
+    // 应用 ID
+    'agent_id'               => '',
+    // 应用回调 AES Key
+    'aes_key'                => '',
+    // 应用回调 Token
+    'token'                  => '',
+    // 加营组 ID
+    'join_training_group_id' => ''
+];
